6f610fcfa70c9bb8af666c325da6a9499a434479
ArnoldaZ <c.arnas123@gmail.com>
2024-01-30 00:17:14 +0200

    add 2022 day 1 solution

--- 2022/01/01.py
+++ 2022/01/01.py
@@ -1,8 +1,25 @@
 
+def get_grouped_calories_sum(lines: list[str]) -> list[int]:
+    groups: list[int] = []
+    current_group: list[int] = []
+
+    for line in lines:
+        if not line:
+            groups.append(sum(current_group))
+            current_group = []
+            continue
+
+        current_group.append(int(line))
+
+    if len(current_group) > 0:
+        groups.append(sum(current_group))
+
+    return groups
+
 def silver_solution(lines: list[str]) -> int:
-    # Add solution
-    return -123
+    groups = get_grouped_calories_sum(lines)
+    return max(groups)
 
 def gold_solution(lines: list[str]) -> int:
-    # Add solution
-    return -321
+    groups = get_grouped_calories_sum(lines)
+    return sum(sorted(groups, reverse=True)[:3])
