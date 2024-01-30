f68d06307f9008bbdc5b0641ce2e1aa8a64505a6
ArnoldaZ <c.arnas123@gmail.com>
2024-01-30 19:43:18 +0200

    small day 3 cleanup

--- 2022/03.py
+++ 2022/03.py
@@ -1,21 +1,19 @@
 
 def get_letter_priority(letter: str) -> int:
     ascii_number = ord(letter)
-    priority = 0
 
     # Upper case letter, need to be converted to range 27-52
     if 65 <= ascii_number <= 90:
-        priority = ascii_number - 38
+        return ascii_number - 38
 
     # Lower case letter, need to be converted to range 1-26
     if 97 <= ascii_number <= 122:
-        priority = ascii_number - 96
+        return ascii_number - 96
 
-    return priority
+    return -1 # impossible to reach
 
 def silver_solution(lines: list[str]) -> int:
     priority_sum = 0
-
     for line in lines:
         half_line_length = len(line) // 2
         first_half, second_half = line[:half_line_length], line[half_line_length:]
@@ -26,7 +24,6 @@ def silver_solution(lines: list[str]) -> int:
 
 def gold_solution(lines: list[str]) -> int:
     priority_sum = 0
-
     for line1, line2, line3 in zip(*[iter(lines)] * 3):
         common_letter = list(set(line1) & set(line2) & set(line3))[0]
         priority_sum += get_letter_priority(common_letter)
