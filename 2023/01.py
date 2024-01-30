a49a825b9a0fb2c8336438cc72346a361989b883
ArnoldaZ <c.arnas123@gmail.com>
2024-01-30 20:50:59 +0200

    simplify 2023 day 1 solutions

--- 2023/01.py
+++ 2023/01.py
@@ -1,37 +1,16 @@
 
-digit_words = [
-    "zero",
-    "one",
-    "two",
-    "three",
-    "four",
-    "five",
-    "six",
-    "seven",
-    "eight",
-    "nine",
-]
+DIGIT_WORDS = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
 def silver_solution(lines: list[str]) -> int:
-    final_value = 0
-    for line in lines:
-        digit_map = [int(digit) for digit in line if digit.isdigit()]
-        value = digit_map[0] * 10 + digit_map[-1]
-        final_value += value
-
-    return final_value
+    digit_lists = [[int(digit) for digit in line if digit.isdigit()] for line in lines]
+    return sum(digit_map[0] * 10 + digit_map[-1] for digit_map in digit_lists)
 
 def gold_solution(lines: list[str]) -> int:
     final_value = 0
-
     for line in lines:
-        digit_list = []
+        digit_list = [(index, int(digit)) for index, digit in enumerate(line) if digit.isdigit()]
 
-        digit_map = [(index, int(digit)) for index, digit in enumerate(line) if digit.isdigit()]
-        for digit in digit_map:
-            digit_list.append(digit)
-
-        for i, digit_word in enumerate(digit_words):
+        for i, digit_word in enumerate(DIGIT_WORDS):
             index = 0
             while index < len(line):
                 index = line.find(digit_word, index)
@@ -41,8 +20,7 @@ def gold_solution(lines: list[str]) -> int:
                 digit_list.append((index, i))
                 index += len(digit_word)
 
-        digit_list.sort(key=lambda tuple: tuple[0])
-
+        digit_list.sort(key=lambda digit_tuple: digit_tuple[0])
         value = digit_list[0][1] * 10 + digit_list[-1][1]
         final_value += value
 
