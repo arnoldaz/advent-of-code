e862b4210971d5e1bcee66fe30ea05fc381fbbe8
ArnoldaZ <c.arnas123@gmail.com>
2024-02-05 00:19:34 +0200

    refactor 2023 day 15

--- 2023/15.py
+++ 2023/15.py
@@ -1,76 +1,46 @@
+from functools import reduce
+
 def parse_input(lines: list[str]) -> list[str]:
-    line = lines[0] # only single line
-    return line.split(",")
+    return lines[0].split(",")
 
 def calc_hash(step: str) -> int:
-    current_value = 0
-    for char in step:
-        current_value += ord(char)
-        current_value *= 17
-        current_value %= 256
-    return current_value
+    return reduce(lambda value, char: (value + ord(char)) * 17 % 256, step, 0)
 
 def ascii_calculation(sequence: list[str]) -> int:
-    final_value = 0
-    for step in sequence:
-        final_value += calc_hash(step)
-    return final_value
+    return sum(calc_hash(step) for step in sequence)
 
 def hashmap_calculation(sequence: list[str]) -> int:
     hashmap: dict[int, list[tuple[str, int]]] = {}
 
     for step in sequence:
-        equals = step.split("=")
-        if len(equals) > 1:
-            index = calc_hash(equals[0])
-            value = int(equals[1])
-            if index in hashmap:
-                for i, iter_value in enumerate(hashmap[index]):
-                    first, _ = iter_value
-                    if first == equals[0]:
-                        found = True
-                        secondary_index = i
-                        break
-                else:
-                    found = False
-                    secondary_index = -1 # impossible to use
+        if "=" in step:
+            label, value = step.split("=")
+            box_index = calc_hash(label)
+            focal_length = int(value)
+            if box_index not in hashmap:
+                hashmap[box_index] = [(label, focal_length)]
+                continue
 
-                if not found:
-                    hashmap[index].append((equals[0], value))
-                else:
-                    hashmap[index][secondary_index] = (equals[0], value)
+            for i, (existing_box_label, _) in enumerate(hashmap[box_index]):
+                if existing_box_label == label:
+                    hashmap[box_index][i] = (label, focal_length)
+                    break
             else:
-                hashmap[index] = [(equals[0], value)]
-
+                hashmap[box_index].append((label, focal_length))
             continue
 
-        equals = step.split("-")
-        if len(equals) > 1:
-            index = calc_hash(equals[0])
-            if index in hashmap:
-                for i, iter_value in enumerate(hashmap[index]):
-                    first, _ = iter_value
-                    if first == equals[0]:
-                        found = True
-                        secondary_index = i
-                        break
-                else:
-                    found = False
-                    secondary_index = -1 # impossible to use
+        if "-" in step:
+            label, _ = step.split("-")
+            box_index = calc_hash(label)
+            if box_index not in hashmap:
+                continue
 
-                if found:
-                    hashmap[index].pop(secondary_index)
+            for i, (existing_box_label, _) in enumerate(hashmap[box_index]):
+                if existing_box_label == label:
+                    hashmap[box_index].pop(i)
+                    break
 
-            continue
-
-    final_value = 0
-
-    for key in hashmap:
-        value_array = hashmap[key]
-        for i, value in enumerate(value_array):
-            final_value += (key + 1) * (i + 1) * value[1]
-
-    return final_value
+    return sum((key + 1) * (i + 1) * focal_length for key, value_array in hashmap.items() for i, (_, focal_length) in enumerate(value_array))
 
 def silver_solution(lines: list[str]) -> int:
     sequence = parse_input(lines)
