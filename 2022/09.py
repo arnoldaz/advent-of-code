fff66a7b70082361a46dc6d9e279a47ed7ea8539
ArnoldaZ <c.arnas123@gmail.com>
2024-02-05 22:33:02 +0200

    refactor 2022 day 9 to use the same function for both solutions

--- 2022/09.py
+++ 2022/09.py
@@ -37,43 +37,25 @@ def get_diagonal_step(head_position: Point, tail_position: Point) -> Point:
 
     return Point(x_diff, y_diff)
 
-def perform_steps(steps: list[tuple[Direction, int]]) -> int:
-    head_position, tail_position = Point(0, 0), Point(0, 0)
-    tail_position_set: set[Point] = set([tail_position])
+def count_tail_unique_positions(steps: list[tuple[Direction, int]], knot_count: int) -> int:
+    knot_positions = [Point(0, 0) for _ in range(knot_count)]
+    tail_positions = set([knot_positions[-1]])
 
     for (step_direction, step_count) in steps:
         for _ in range(step_count):
-            head_position += step_direction
-            if not is_tail_adjacent(head_position, tail_position):
-                tail_position += get_diagonal_step(head_position, tail_position)
-                tail_position_set.add(tail_position)
+            knot_positions[0] += step_direction
+            for i in range(1, len(knot_positions)):
+                if not is_tail_adjacent(knot_positions[i-1], knot_positions[i]):
+                    knot_positions[i] += get_diagonal_step(knot_positions[i-1], knot_positions[i])
 
-    return len(tail_position_set)
+            tail_positions.add(knot_positions[-1])
 
-def perform_steps_gold(steps: list[tuple[Direction, int]], tail_count: int) -> int:
-    tail_positions = [Point(0, 0) for _ in range(tail_count + 1)]
-    last_tail_position_set: list[Point] = list([tail_positions[-1]])
-
-    for (step_direction, step_count) in steps:
-        for _ in range(step_count):
-            tail_positions[0] += step_direction
-
-            for i in range(1, len(tail_positions)):
-                if not is_tail_adjacent(tail_positions[i-1], tail_positions[i]):
-                    tail_positions[i] += get_diagonal_step(tail_positions[i-1], tail_positions[i])
-                    if i == len(tail_positions) - 1:
-                        last_tail_position_set.append(tail_positions[i])
-
-    return len(set(last_tail_position_set))
+    return len(tail_positions)
 
 def silver_solution(lines: list[str]) -> int:
     steps = parse_input(lines)
-    result = perform_steps(steps)
-
-    return result
+    return count_tail_unique_positions(steps, 2)
 
 def gold_solution(lines: list[str]) -> int:
     steps = parse_input(lines)
-    result = perform_steps_gold(steps, 9)
-
-    return result
+    return count_tail_unique_positions(steps, 10)
