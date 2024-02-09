31542a13dc9ad1d0e7b42f22c68b981becf52c33
ArnoldaZ <c.arnas123@gmail.com>
2024-02-09 18:01:46 +0200

    refactor 2022 day 14 solutions

--- 2022/14.py
+++ 2022/14.py
@@ -1,35 +1,19 @@
-# pylint: disable=unused-argument
-
 import sys
 from utils.matrix import Matrix
 from utils.point import Direction, Point
 
-from colorama import Fore
-
 WALL_SYMBOL = 1
 SAND_SYMBOL = 2
 
-def parse_input(lines: list[str]) -> Matrix[int]:
-    width, height = 600, 600
-    data = [[0 for _ in range(width)] for _ in range(height)]
+# Safe assumptions by how much to increase initial matrix for incoming sand
+LEEWAY_TILE_COUNT = {
+    Direction.UP: 50,
+    Direction.DOWN: 5,
+    Direction.LEFT: 200,
+    Direction.RIGHT: 200,
+}
 
-    for line in lines:
-        corners = [Point(int(x), int(y)) for corner_pair in line.split(" -> ") for x, y in [corner.split(",") for corner in corner_pair.split()]]
-        for corner1, corner2 in zip(corners, corners[1:]):
-            if corner1.x == corner2.x:
-                for y in range(min(corner1.y, corner2.y), max(corner1.y, corner2.y) + 1):
-                    data[y][corner1.x] = WALL_SYMBOL
-            elif corner1.y == corner2.y:
-                for x in range(min(corner1.x, corner2.x), max(corner1.x, corner2.x) + 1):
-                    data[corner1.y][x] = WALL_SYMBOL
-            else:
-                raise ValueError("Impossible input")
-
-    return Matrix[int](data, int)
-
-LEEWAY_COUNT = 400
-
-def parse_input2(lines: list[str]) -> tuple[Matrix[int], Point]:
+def parse_input(lines: list[str], add_ground: bool) -> tuple[Matrix[int], Point]:
     map_lines: list[tuple[Point, Point]] = []
 
     for line in lines:
@@ -44,29 +28,15 @@ def parse_input2(lines: list[str]) -> tuple[Matrix[int], Point]:
 
     max_x, max_y, min_x, min_y = 0, 0, sys.maxsize, sys.maxsize
     for start, end in map_lines:
-        if start.x < min_x:
-            min_x = start.x
-        if start.x > max_x:
-            max_x = start.x
-        if end.x < min_x:
-            min_x = end.x
-        if end.x > max_x:
-            max_x = end.x
-        if start.y < min_y:
-            min_y = start.y
-        if start.y > max_y:
-            max_y = start.y
-        if end.y < min_y:
-            min_y = end.y
-        if end.y > max_y:
-            max_y = end.y
+        min_x = min(min_x, start.x, end.x)
+        max_x = max(max_x, start.x, end.x)
+        min_y = min(min_y, start.y, end.y)
+        max_y = max(max_y, start.y, end.y)
 
-    # print(max_x, max_y, min_x, min_y)
-
-    width = max_x - min_x + LEEWAY_COUNT
-    height = max_y - min_y + LEEWAY_COUNT
+    width = max_x - min_x + LEEWAY_TILE_COUNT[Direction.LEFT] + LEEWAY_TILE_COUNT[Direction.RIGHT]
+    height = max_y - min_y + LEEWAY_TILE_COUNT[Direction.UP] + LEEWAY_TILE_COUNT[Direction.DOWN]
     data = [[0 for _ in range(width)] for _ in range(height)]
-    offset = Point(min_x - LEEWAY_COUNT // 2, min_y - LEEWAY_COUNT // 2)
+    offset = Point(min_x - LEEWAY_TILE_COUNT[Direction.LEFT], min_y - LEEWAY_TILE_COUNT[Direction.UP])
 
     for start, end in map_lines:
         if start.x == end.x:
@@ -78,34 +48,14 @@ def parse_input2(lines: list[str]) -> tuple[Matrix[int], Point]:
         else:
             raise ValueError("Impossible input")
 
-    for x in range(width):
-        data[max_y + 2 - offset.y][x] = WALL_SYMBOL
+    if add_ground:
+        for x in range(width):
+            data[max_y + 2 - offset.y][x] = WALL_SYMBOL
 
     return Matrix[int](data, int), offset
 
-
-def drop_sand(matrix: Matrix[int], sand_start: Point) -> bool:
-    column = matrix.get_column(sand_start.x)
-    first_obstacle_y = next((i for i, value in enumerate(column) if value > 0 and i > sand_start.y), None)
-    if first_obstacle_y is None:
-        return False
-
-    first_obstacle = Point(sand_start.x, first_obstacle_y)
-
-    obstacle_left = first_obstacle + Direction.LEFT
-    if matrix.get_symbol(obstacle_left) == 0:
-        return drop_sand(matrix, obstacle_left)
-
-    obstacle_right = first_obstacle + Direction.RIGHT
-    if matrix.get_symbol(obstacle_right) == 0:
-        return drop_sand(matrix, obstacle_right)
-
-    matrix.set_symbol(first_obstacle + Direction.UP, SAND_SYMBOL)
-    return True
-
-def drop_sand_offset(matrix: Matrix[int], sand_start: Point, offset: Point) -> bool:
+def drop_sand(matrix: Matrix[int], sand_start: Point, offset: Point) -> bool:
     sand_start -= offset
-
     if matrix.get_symbol(sand_start) == SAND_SYMBOL:
         return False
 
@@ -118,84 +68,42 @@ def drop_sand_offset(matrix: Matrix[int], sand_start: Point, offset: Point) -> b
 
     obstacle_left = first_obstacle + Direction.LEFT
     if matrix.get_symbol(obstacle_left) == 0:
-        return drop_sand(matrix, obstacle_left)
+        return drop_sand(matrix, obstacle_left, Point())
 
     obstacle_right = first_obstacle + Direction.RIGHT
     if matrix.get_symbol(obstacle_right) == 0:
-        return drop_sand(matrix, obstacle_right)
+        return drop_sand(matrix, obstacle_right, Point())
 
     matrix.set_symbol(first_obstacle + Direction.UP, SAND_SYMBOL)
     return True
 
-def count_sand(matrix: Matrix[int]) -> int:
-    counter = 0
-    for line in matrix.get_data():
-        for number in line:
-            if number == SAND_SYMBOL:
-                counter += 1
+def calculate_sand_amount(matrix: Matrix[int], offset: Point) -> int:
+    sand_start = Point(500, 0)
+    while drop_sand(matrix, sand_start, offset):
+        pass
 
-    return counter
+    # visualize_matrix(matrix)
+    return sum(number == SAND_SYMBOL for line in matrix.get_data() for number in line)
+
+def visualize_matrix(matrix: Matrix[int]):
+    from colorama import Fore # pylint: disable=import-outside-toplevel
+
+    height, width = matrix.height(), matrix.width()
+    for y in range(height):
+        for x in range(width):
+            symbol = matrix.get_symbol(Point(x, y))
+            if symbol == WALL_SYMBOL:
+                print(f"{Fore.BLUE}0{Fore.RESET}", end="")
+            elif symbol == SAND_SYMBOL:
+                print(f"{Fore.RED}0{Fore.RESET}", end="")
+            else:
+                print("0", end="")
+        print()
 
 def silver_solution(lines: list[str]) -> int:
-    matrix = parse_input(lines)
-    sand_start = Point(500, 0)
-
-    while drop_sand(matrix, sand_start):
-        # for y in range(15):
-        #     for x in range(490, 510):
-        #         symbol = matrix.get_symbol(Point(x, y))
-        #         if symbol == 1:
-        #             print(f"{Fore.BLUE}{symbol}{Fore.RESET}", end="")
-        #         elif symbol == 2:
-        #             print(f"{Fore.RED}{symbol}{Fore.RESET}", end="")
-        #         else:
-        #             print(symbol, end="")
-        #     print()
-        pass
-
-    for y in range(matrix.height()):
-        for x in range(matrix.width()):
-            symbol = matrix.get_symbol(Point(x, y))
-            if symbol == 1:
-                print(f"{Fore.BLUE}{symbol}{Fore.RESET}", end="")
-            elif symbol == 2:
-                print(f"{Fore.RED}{symbol}{Fore.RESET}", end="")
-            else:
-                print(symbol, end="")
-        print()
-    print("==============================================")
-
-    return count_sand(matrix)
+    matrix, offset = parse_input(lines, False)
+    return calculate_sand_amount(matrix, offset)
 
 def gold_solution(lines: list[str]) -> int:
-    matrix, offset = parse_input2(lines)
-    sand_start = Point(500, 0)
-
-    while drop_sand_offset(matrix, sand_start, offset):
-        # for y in range(matrix.height()):
-        #     for x in range(matrix.width()):
-        #         symbol = matrix.get_symbol(Point(x, y))
-        #         if symbol == 1:
-        #             print(f"{Fore.BLUE}{symbol}{Fore.RESET}", end="")
-        #         elif symbol == 2:
-        #             print(f"{Fore.RED}{symbol}{Fore.RESET}", end="")
-        #         else:
-        #             print(symbol, end="")
-        #     print()
-        # print("==============================================")
-        pass
-
-
-    for y in range(matrix.height()):
-        for x in range(matrix.width()):
-            symbol = matrix.get_symbol(Point(x, y))
-            if symbol == 1:
-                print(f"{Fore.BLUE}{symbol}{Fore.RESET}", end="")
-            elif symbol == 2:
-                print(f"{Fore.RED}{symbol}{Fore.RESET}", end="")
-            else:
-                print(symbol, end="")
-        print()
-    print("==============================================")
-
-    return count_sand(matrix)
+    matrix, offset = parse_input(lines, True)
+    return calculate_sand_amount(matrix, offset)
