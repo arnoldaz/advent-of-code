cef0fe57e0254f42a90153c1668a54446b25037d
ArnoldaZ <c.arnas123@gmail.com>
2024-02-09 18:09:46 +0200

    fix pylint ignore comment

--- 2022/14.py
+++ 2022/14.py
@@ -86,7 +86,8 @@ def calculate_sand_amount(matrix: Matrix[int], offset: Point) -> int:
     return sum(number == SAND_SYMBOL for line in matrix.get_data() for number in line)
 
 def visualize_matrix(matrix: Matrix[int]):
-    from colorama import Fore # pylint: disable=import-outside-toplevel
+    # pylint: disable-next=import-outside-toplevel
+    from colorama import Fore
 
     height, width = matrix.height(), matrix.width()
     for y in range(height):
