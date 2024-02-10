02c41c55c1bed61b368afecc93a240cc6c82afc4
ArnoldaZ <c.arnas123@gmail.com>
2024-02-10 02:54:30 +0200

    add 2022 day 15 gold solution

--- 2022/15.py
+++ 2022/15.py
@@ -1,4 +1,4 @@
-from typing import NamedTuple
+from typing import Iterator, NamedTuple
 from utils.point import Point
 from utils.string import get_ints
 
@@ -40,5 +40,38 @@ def silver_solution(lines: list[str]) -> int:
 
     return counter
 
+def get_outside_sensor_path(sensor: Sensor) -> Iterator[Point]:
+    distance = manhattan_distance(sensor.location, sensor.closest_beacon)
+    left = Point(sensor.location.x - distance - 1, sensor.location.y)
+    right = Point(sensor.location.x + distance + 1, sensor.location.y)
+    up = Point(sensor.location.x, sensor.location.y - distance - 1)
+    down = Point(sensor.location.x, sensor.location.y + distance + 1)
+
+    left_up, up_right, right_down, down_left = Point(1, -1), Point(1, 1), Point(-1, 1), Point(-1, -1)
+    current_left, current_up, current_right, current_down = left, up, right, down
+
+    while current_left != up:
+        current_left += left_up
+        current_up += up_right
+        current_right += right_down
+        current_down += down_left
+        yield current_left
+        yield current_up
+        yield current_right
+        yield current_down
+
 def gold_solution(lines: list[str]) -> int:
+    sensors = parse_input(lines)
+    building_locations = set(sensor.closest_beacon for sensor in sensors) | set(sensor.location for sensor in sensors)
+    distances = [manhattan_distance(sensor.location, sensor.closest_beacon) for sensor in sensors]
+
+    for sensor in sensors:
+        for point in get_outside_sensor_path(sensor):
+            if point in building_locations:
+                continue
+
+            blocked = any(manhattan_distance(point, sensor.location) <= distances[i] for i, sensor in enumerate(sensors))
+            if not blocked and 0 <= point.x <= 4_000_000 and 0 <= point.y <= 4_000_000:
+                return point.x * 4_000_000 + point.y
+
     return -1
