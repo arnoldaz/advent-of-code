c3357dfcdc07b8630f7b7a7e911b5b72c2cee8b9
ArnoldaZ <c.arnas123@gmail.com>
2024-02-14 23:05:57 +0200

    some more 2023 day 20 cleanups

--- 2023/20.py
+++ 2023/20.py
@@ -32,9 +32,9 @@ def parse_input(lines: list[str]) -> dict[str, Module]:
 
     return modules
 
-def calculate_pulses_recursive(modules_and_pulses: list[tuple[Module, bool]], mapp: dict[str, Module], flip_flop_states: dict[str, bool], conjunction_states: dict[str, list[tuple[str, bool]]], button_counter: int) -> tuple[int, int]:
+def calculate_pulses_recursive(modules_and_pulses: list[tuple[Module, bool]], modules: dict[str, Module], flip_flop_states: dict[str, bool], conjunction_states: dict[str, list[tuple[str, bool]]]) -> tuple[int, int]:
     low_pulse_count, high_pulse_count = 0, 0
-    next_modules = []
+    next_modules: list[tuple[Module, bool]] = []
     for module_and_pulse in modules_and_pulses:
         module, pulse = module_and_pulse
         for module_name in module.destinations:
@@ -43,14 +43,14 @@ def calculate_pulses_recursive(modules_and_pulses: list[tuple[Module, bool]], ma
             else:
                 low_pulse_count += 1
 
-            if module_name not in mapp:
+            if module_name not in modules:
                 # print(f"{module.variable} -{"high" if pulse else "low"}-> {module_name}")
                 continue
 
-            new_module = mapp[module_name]
+            new_module = modules[module_name]
+            # print(f"{module.variable} -{"high" if pulse else "low"}-> {new_module.variable}")
             match new_module.type:
                 case ModuleType.NORMAL:
-                    # print(f"{module.variable} -{"high" if pulse else "low"}-> {new_module.variable}")
                     new_pulse = pulse
                 case ModuleType.FLIP_FLOP:
                     if not pulse:
@@ -58,30 +58,24 @@ def calculate_pulses_recursive(modules_and_pulses: list[tuple[Module, bool]], ma
                         new_state = not old_state
                         flip_flop_states[new_module.variable] = new_state
 
-                        # print(f"{module.variable} -{"high" if pulse else "low"}-> {new_module.variable}")
-                        if old_state:
-                            new_pulse = False
-                        else:
-                            new_pulse = True
+                        new_pulse = not old_state
                     else:
-                        # print(f"{module.variable} -{"high" if pulse else "low"}-> {new_module.variable}")
                         continue
                 case ModuleType.CONJUNCTION:
                     state_list = conjunction_states[new_module.variable]
-                    for i, state in enumerate(state_list):
-                        if state[0] == module.variable:
-                            state_list[i] = (state[0], pulse)
+                    for i, (state_name, _) in enumerate(state_list):
+                        if state_name == module.variable:
+                            state_list[i] = (state_name, pulse)
 
-                    # print(f"{module.variable} -{"high" if pulse else "low"}-> {new_module.variable}")
-                    if all(state[1] for state in state_list):
+                    if all(state_pulse for (_, state_pulse) in state_list):
                         new_pulse = False
                     else:
                         new_pulse = True
 
             next_modules.append((new_module, new_pulse))
 
-    if len(next_modules) != 0:
-        child_low_pulse_count, child_high_pulse_count = calculate_pulses_recursive(next_modules, mapp, flip_flop_states, conjunction_states, button_counter)
+    if len(next_modules) > 0:
+        child_low_pulse_count, child_high_pulse_count = calculate_pulses_recursive(next_modules, modules, flip_flop_states, conjunction_states)
         low_pulse_count += child_low_pulse_count
         high_pulse_count += child_high_pulse_count
 
@@ -96,11 +90,11 @@ def calculate_pulses(modules: dict[str, Module]):
 
     total_low_pulse_count, total_high_pulse_count = 0, 0
 
-    for i in range(1_000):
+    for _ in range(1_000):
         total_low_pulse_count += 1
-
         # print(f"button -{"high" if start_pulse else "low"}-> {start_module.variable}")
-        low_pulse_count, high_pulse_count = calculate_pulses_recursive([(start_module, start_pulse)], modules, flip_flop_states, conjunction_states, i)
+
+        low_pulse_count, high_pulse_count = calculate_pulses_recursive([(start_module, start_pulse)], modules, flip_flop_states, conjunction_states)
         total_low_pulse_count += low_pulse_count
         total_high_pulse_count += high_pulse_count
 
