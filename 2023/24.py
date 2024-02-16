535b8479ef5402abefbb026bad46aeaaaccdf54c
ArnoldaZ <c.arnas123@gmail.com>
2024-02-16 21:01:24 +0200

    refactor 2023 day 24 to work with build system

--- 2023/24.py
+++ 2023/24.py
@@ -1,47 +1,7 @@
-from enum import Enum
-from typing import NamedTuple, Optional
-import uuid
-import sys
+from decimal import DivisionByZero
+from typing import NamedTuple
 import math
-
-# sys.setrecursionlimit(2 ** 30)
-
-with open("input.txt") as file:
-    lines = [line.rstrip() for line in file]
-
-class Point:
-    x: int
-    y: int
-    z: int
-
-    def __init__(self, x: int = 0, y: int = 0, z: int = 0):
-        self.x = x
-        self.y = y
-        self.z = z
-
-    def __str__(self):
-        return f"{{Point: x={self.x}, y={self.y}, z={self.z}}}"
-
-    def __repr__(self):
-        return self.__str__()
-
-    def __hash__(self):
-        return hash((self.x, self.y, self.z))
-
-    def __add__(self, other):
-        if isinstance(other, Point):
-            return Point(self.x + other.x, self.y + other.y, self.z + other.z)
-        
-        if isinstance(other, int):
-            return Point(self.x + other, self.y + other, self.z + other)
-        
-        raise Exception(f"Unrecognized variable added to Point - {other}")
-
-    def __eq__(self, other):
-        if isinstance(other, Point):
-            return self.x == other.x and self.y == other.y and self.z == other.z
-        
-        raise Exception(f"Unrecognized variable compared to Point - {other}")
+from utils.point import Point
 
 class Hailstone(NamedTuple):
     position: Point
@@ -71,9 +31,9 @@ def generate_2d_intersection(hailstone1: Hailstone, hailstone2: Hailstone) -> tu
     y3, y4 = hailstone2.position.y, hailstone2.position.y + hailstone2.velocity.y
 
     try:
-        px = ( (x1*y2-y1*x2)*(x3-x4)-(x1-x2)*(x3*y4-y3*x4) ) / ( (x1-x2)*(y3-y4)-(y1-y2)*(x3-x4) ) 
+        px = ( (x1*y2-y1*x2)*(x3-x4)-(x1-x2)*(x3*y4-y3*x4) ) / ( (x1-x2)*(y3-y4)-(y1-y2)*(x3-x4) )
         py = ( (x1*y2-y1*x2)*(y3-y4)-(y1-y2)*(x3*y4-y3*x4) ) / ( (x1-x2)*(y3-y4)-(y1-y2)*(x3-x4) )
-    except Exception:
+    except ZeroDivisionError:
         return (math.inf, math.inf)
 
     return (px, py)
@@ -91,13 +51,13 @@ def is_point_in_direction(start_point: tuple[float, float], direction_vector: tu
 def count_intersections_in_area(hailstones: list[Hailstone], test_area_min: int, test_area_max: int) -> int:
     intersections = 0
 
-    for i in range(len(hailstones)):
+    for i in range(len(hailstones)): # pylint: disable=consider-using-enumerate
         for j in range(i + 1, len(hailstones)):
             point = generate_2d_intersection(hailstones[i], hailstones[j])
             if point[0] == math.inf or point[1] == math.inf:
                 # print("inf")
                 continue
-            
+
             if test_area_min <= point[0] <= test_area_max and test_area_min <= point[1] <= test_area_max:
                 if not is_point_in_direction((hailstones[i].position.x, hailstones[i].position.y), (hailstones[i].velocity.x, hailstones[i].velocity.y), point):
                     # print("behind first")
@@ -111,9 +71,13 @@ def count_intersections_in_area(hailstones: list[Hailstone], test_area_min: int,
                 intersections += 1
             # else:
             #     print(f"not adding {point}")
-            
 
     return intersections
 
-_hailstones = get_initial_data(lines, True)
-print(f"{count_intersections_in_area(_hailstones, 200000000000000, 400000000000000)=}")
+def silver_solution(lines: list[str]) -> int:
+    _hailstones = get_initial_data(lines, True)
+    return count_intersections_in_area(_hailstones, 200000000000000, 400000000000000)
+
+def gold_solution(_lines: list[str]) -> int:
+    # Implement solution
+    return -321
