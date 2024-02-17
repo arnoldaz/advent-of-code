0af4d2c5e60ef0685e03570d665e66b7d5173776
ArnoldaZ <c.arnas123@gmail.com>
2024-02-17 02:29:29 +0200

    add 2023 day 24 gold solution using sympy

--- 2023/24.py
+++ 2023/24.py
@@ -1,6 +1,6 @@
-from decimal import DivisionByZero
 from typing import NamedTuple
 import math
+import sympy
 from utils.point import Point
 
 class Hailstone(NamedTuple):
@@ -43,7 +43,6 @@ def dot_product(vector1: tuple[float, float], vector2: tuple[float, float]) -> f
 
 def is_point_in_direction(start_point: tuple[float, float], direction_vector: tuple[float, float], test_point: tuple[float, float], threshold: float = 0.99) -> bool:
     start_test_vector = tuple[float, float](test - start for start, test in zip(start_point, test_point))
-
     dot_product_value = dot_product(direction_vector, start_test_vector)
 
     return dot_product_value > threshold
@@ -51,33 +50,37 @@ def is_point_in_direction(start_point: tuple[float, float], direction_vector: tu
 def count_intersections_in_area(hailstones: list[Hailstone], test_area_min: int, test_area_max: int) -> int:
     intersections = 0
 
-    for i in range(len(hailstones)): # pylint: disable=consider-using-enumerate
-        for j in range(i + 1, len(hailstones)):
-            point = generate_2d_intersection(hailstones[i], hailstones[j])
-            if point[0] == math.inf or point[1] == math.inf:
-                # print("inf")
+    for i, hailstone1 in enumerate(hailstones):
+        for hailstone2 in hailstones[i+1:]:
+            point_x, point_y = generate_2d_intersection(hailstone1, hailstone2)
+            if math.inf in (point_x, point_y):
                 continue
 
-            if test_area_min <= point[0] <= test_area_max and test_area_min <= point[1] <= test_area_max:
-                if not is_point_in_direction((hailstones[i].position.x, hailstones[i].position.y), (hailstones[i].velocity.x, hailstones[i].velocity.y), point):
-                    # print("behind first")
-                    continue
+            if test_area_min <= point_x <= test_area_max and test_area_min <= point_y <= test_area_max:
+                if not is_point_in_direction((hailstone1.position.x, hailstone1.position.y), (hailstone1.velocity.x, hailstone1.velocity.y), (point_x, point_y)):
+                    continue # behind first hailstone
 
-                if not is_point_in_direction((hailstones[j].position.x, hailstones[j].position.y), (hailstones[j].velocity.x, hailstones[j].velocity.y), point):
-                    # print("behind second")
-                    continue
+                if not is_point_in_direction((hailstone2.position.x, hailstone2.position.y), (hailstone2.velocity.x, hailstone2.velocity.y), (point_x, point_y)):
+                    continue # behind second hailstone
 
-                # print(f"adding intersection {point}")
                 intersections += 1
-            # else:
-            #     print(f"not adding {point}")
 
     return intersections
 
 def silver_solution(lines: list[str]) -> int:
-    _hailstones = get_initial_data(lines, True)
-    return count_intersections_in_area(_hailstones, 200000000000000, 400000000000000)
+    hailstones = get_initial_data(lines, True)
+    return count_intersections_in_area(hailstones, 200_000_000_000_000, 400_000_000_000_000)
 
-def gold_solution(_lines: list[str]) -> int:
-    # Implement solution
-    return -321
+def gold_solution(lines: list[str]) -> int:
+    hailstones = get_initial_data(lines, False)
+    x, y, z, vx, vy, vz = (sympy.Symbol(var) for var in ["x", "y", "z", "vx", "vy", "vz"])
+    variables = [x, y, z, vx, vy, vz]
+    equations = []
+    for i, hailstone in enumerate(hailstones[:3]):
+        t = sympy.Symbol(f"t_{i}")
+        equations.append(x + vx * t - (hailstone.position.x + hailstone.velocity.x * t)) # type: ignore reportOperatorIssue
+        equations.append(y + vy * t - (hailstone.position.y + hailstone.velocity.y * t)) # type: ignore reportOperatorIssue
+        equations.append(z + vz * t - (hailstone.position.z + hailstone.velocity.z * t)) # type: ignore reportOperatorIssue
+        variables.append(t)
+
+    return int(sum(sympy.solve_poly_system(equations, variables)[0][:3])) # type: ignore reportOptionalSubscript
