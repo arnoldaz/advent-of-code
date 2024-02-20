9e1367b564e1d338ced530c2e1a2c6e707dfb4cb
ArnoldaZ <c.arnas123@gmail.com>
2024-02-20 22:25:06 +0200

    add 2022 day 18 gold solution

--- 2022/18.py
+++ 2022/18.py
@@ -1,16 +1,12 @@
-# pylint: disable=unused-argument
-
 import sys
-from utils.point import INVALID_POINT, Point
+from utils.point import Point
 
 sys.setrecursionlimit(2 ** 30)
 
 def parse_input(lines: list[str]) -> list[Point]:
     return [Point(*(int(coord) for coord in line.split(","))) for line in lines]
 
-def silver_solution(lines: list[str]) -> int:
-    points = parse_input(lines)
-
+def calculate_surface_area(points: list[Point]) -> int:
     neighbors_map: dict[Point, int] = {}
     surface_area = 0
 
@@ -28,119 +24,55 @@ def silver_solution(lines: list[str]) -> int:
 
     return surface_area
 
-def flood_fill_3d(points: list[Point], location: Point, filled_points: list[Point]):
+def flood_fill(points: set[Point], location: Point, filled_points: set[Point]):
     if location in points or location in filled_points:
         return
 
-    print(location)
-
-    filled_points.append(location)
+    filled_points.add(location)
     for neighbor in location.get_neighbors_3d():
-        flood_fill_3d(points, neighbor, filled_points)
+        flood_fill(points, neighbor, filled_points)
 
-def is_inside_point(point_to_check: Point, points: list[Point]) -> bool:
-    x_less = any(point.x < point_to_check.x and point.y == point_to_check.y and point.z == point_to_check.z for point in points)
-    x_more = any(point.x > point_to_check.x and point.y == point_to_check.y and point.z == point_to_check.z for point in points)
+def get_outer_border(points: list[Point]) -> tuple[set[Point], Point, Point]:
+    outer_border = set[Point]()
 
-    y_less = any(point.y < point_to_check.y and point.x == point_to_check.x and point.z == point_to_check.z for point in points)
-    y_more = any(point.y > point_to_check.y and point.x == point_to_check.x and point.z == point_to_check.z for point in points)
+    min_x, max_x, min_y, max_y, min_z, max_z = sys.maxsize, 0, sys.maxsize, 0, sys.maxsize, 0
+    for point in points:
+        min_x = min(min_x, point.x)
+        max_x = max(max_x, point.x)
+        min_y = min(min_y, point.y)
+        max_y = max(max_y, point.y)
+        min_z = min(min_z, point.z)
+        max_z = max(max_z, point.z)
 
-    z_less = any(point.z < point_to_check.z and point.y == point_to_check.y and point.x == point_to_check.x for point in points)
-    z_more = any(point.z > point_to_check.z and point.y == point_to_check.y and point.x == point_to_check.x for point in points)
+    outer_min_x, outer_max_x = min_x - 2, max_x + 2
+    outer_min_y, outer_max_y = min_y - 2, max_y + 2
+    outer_min_z, outer_max_z = min_z - 2, max_z + 2
 
-    return x_less and x_more and y_less and y_more and z_less and z_more
+    for x in range(outer_min_x, outer_max_x + 1):
+        for y in range(outer_min_y, outer_max_y + 1):
+            for z in range(outer_min_z, outer_max_z + 1):
+                if x in (outer_min_x, outer_max_x) or y in (outer_min_y, outer_max_y) or z in (outer_min_z, outer_max_z):
+                    outer_border.add(Point(x, y, z))
 
-def get_flood_fill_length(points: list[Point], location: Point):
-    filled_points: list[Point] = []
-    flood_fill_3d(points, location, filled_points)
+    return outer_border, Point(outer_min_x, outer_min_y, outer_min_z), Point(outer_max_x, outer_max_y, outer_max_z)
 
-
-    return len(filled_points)
+def silver_solution(lines: list[str]) -> int:
+    points = parse_input(lines)
+    return calculate_surface_area(points)
 
 def gold_solution(lines: list[str]) -> int:
     points = parse_input(lines)
+    outside_border, border_min_corner, border_max_corner = get_outer_border(points)
 
-    neighbors_map: dict[Point, int] = {}
-    surface_area = 0
+    filled_points = set[Point]()
+    flood_fill(set(points) | outside_border, border_min_corner + 1, filled_points)
 
-    for point in points:
-        surface_area += 6
-        if point in neighbors_map:
-            surface_area -= 2 * neighbors_map[point]
+    combined_area = calculate_surface_area(list(filled_points))
 
-        neighbors = point.get_neighbors_3d()
-        for neighbor in neighbors:
-            if neighbor in neighbors_map:
-                neighbors_map[neighbor] += 1
-            else:
-                neighbors_map[neighbor] = 1
+    border_x = (border_max_corner.x - 1) - (border_min_corner.x + 1) + 1
+    border_y = (border_max_corner.y - 1) - (border_min_corner.y + 1) + 1
+    border_z = (border_max_corner.z - 1) - (border_min_corner.z + 1) + 1
 
-    inside_points = []
+    outer_area = 2 * border_x * border_y + 2 * border_x * border_z + 2 * border_y * border_z
 
-    for neighbor in neighbors_map:
-        if neighbor not in points and is_inside_point(neighbor, points):
-            inside_points.append(neighbor)
-
-    print(len(inside_points))
-
-    for inside in inside_points:
-        print(get_flood_fill_length(points, inside))
-
-    return -1
-
-    inside_point = INVALID_POINT
-
-    for key, value in neighbors_map.items():
-        if value == 6 and key not in points:
-            inside_point = key
-            break
-
-    print(inside_point)
-
-    filled_points: list[Point] = []
-    flood_fill_3d(points, inside_point, filled_points)
-
-    print(filled_points)
-
-    new_neighbors_map: dict[Point, int] = {}
-    new_surface_area = 0
-
-    for point in filled_points:
-        new_surface_area += 6
-        if point in new_neighbors_map:
-            new_surface_area -= 2 * new_neighbors_map[point]
-
-        neighbors = point.get_neighbors_3d()
-        for neighbor in neighbors:
-            if neighbor in new_neighbors_map:
-                new_neighbors_map[neighbor] += 1
-            else:
-                new_neighbors_map[neighbor] = 1
-
-    print(new_surface_area)
-
-    # found_something = True
-
-    # while found_something:
-    #     found_something = False
-    #     new_keys = []
-    #     parsed_key = INVALID_POINT
-
-    #     for key, value in neighbors_map.items():
-    #         if value == 6 and key not in points:
-    #             found_something = True
-    #             surface_area -= 6
-
-    #             new_keys = key.get_neighbors_3d()
-    #             parsed_key = key
-
-    #     if found_something:
-    #         for neighbor in new_keys:
-    #             if neighbor in neighbors_map:
-    #                 neighbors_map[neighbor] += 1
-    #             else:
-    #                 neighbors_map[neighbor] = 1
-
-    #         del neighbors_map[parsed_key]
-
-    return surface_area - new_surface_area
+    return combined_area - outer_area
