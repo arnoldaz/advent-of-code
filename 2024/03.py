405b1bb0056e5db69541efc64d8b78ae1695163c
arnoldaz <60964494+arnoldaz@users.noreply.github.com>
2024-12-03 11:43:46 +0200

    add 2024 day 3 solution

---
+++ 2024/03.py
@@ -0,0 +1,33 @@
+import re
+
+def silver_solution(lines: list[str]) -> int:
+    mul_format = r"mul\((\d+),(\d+)\)"
+    line = "".join(lines)
+
+    matches = re.findall(mul_format, line)
+
+    return sum(int(match[0]) * int(match[1]) for match in matches)
+
+def gold_solution(lines: list[str]) -> int:
+    mul_format = r"mul\(\d+,\d+\)|do\(\)|don't\(\)"
+    inner_mul_format = r"mul\((\d+),(\d+)\)"
+    line = "".join(lines)
+
+    enabled = True
+    final_sum = 0
+
+    matches = re.findall(mul_format, line)
+    for match in matches:
+        if match == "do()":
+            enabled = True
+        elif match == "don't()":
+            enabled = False
+        else:
+            if not enabled:
+                continue
+
+            inner_matches = re.match(inner_mul_format, match)
+            if inner_matches:
+                final_sum += int(inner_matches.group(1)) * int(inner_matches.group(2))
+
+    return final_sum
