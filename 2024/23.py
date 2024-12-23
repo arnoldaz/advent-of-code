208984554adfc85b698d051de7b6e6fabdf711fb
ArnoldaZ <c.arnas123@gmail.com>
2024-12-23 14:41:03 +0200

    add 2024 day 23 solution

---
+++ 2024/23.py
@@ -0,0 +1,22 @@
+import networkx as nx
+
+def parse_input(lines: list[str]) -> dict[str, list[str]]:
+    connections = [tuple[str, str](line.split("-")) for line in lines]
+    groups: dict[str, list[str]] = {}
+    for left, right in connections:
+        groups.setdefault(left, []).append(right)
+        groups.setdefault(right, []).append(left)
+
+    return groups
+
+def silver_solution(lines: list[str]) -> int:
+    graph = parse_input(lines)
+
+    return sum(1 for triangle in nx.enumerate_all_cliques(nx.Graph(graph))
+        if len(triangle) == 3 and any(str(x).startswith("t") for x in triangle))
+
+def gold_solution(lines: list[str]) -> str:
+    graph = parse_input(lines)
+
+    largest_clique = max(nx.find_cliques(nx.Graph(graph)), key=len)
+    return ",".join(sorted(largest_clique))
