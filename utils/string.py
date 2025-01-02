59dac0f2476d04d4b5ac0fa81dadcc211a557d46
ArnoldaZ <c.arnas123@gmail.com>
2025-01-03 00:28:54 +0200

    cleanup 2015 day 19 solutions

--- utils/string.py
+++ utils/string.py
@@ -18,3 +18,8 @@ def get_ints(string: str, allow_negative: bool = False) -> list[int]:
     """Gets all integers from a string using regex"""
     regex = r"\d+" if not allow_negative else r"-*\d+"
     return [int(x) for x in re.findall(regex, string)]
+
+def replace_nth_instance(string: str, old: str, new: str, n: int) -> str:
+    """Replaces Nth instance of substring in a string"""
+    split_string = string.split(old)
+    return f"{old.join(split_string[:n])}{new}{old.join(split_string[n:])}"
