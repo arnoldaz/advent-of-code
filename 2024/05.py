1520a0240d634bc005d32a3e3a9d4c5326345018
ArnoldaZ <c.arnas123@gmail.com>
2025-01-05 23:16:44 +0200

    refactor a bit of 2024 day 5

--- 2024/05.py
+++ 2024/05.py
@@ -1,42 +1,32 @@
-def is_order_correct(order: list[int], rules: list[tuple[int, int]]) -> bool:
-    for rule in rules:
-        if rule[0] not in order or rule[1] not in order:
-            continue
-
-        if not order.index(rule[0]) < order.index(rule[1]):
-            return False
-            
-    return True
- 
 def parse_input(lines: list[str]) -> tuple[list[tuple[int, int]], list[list[int]]]:
-    ordering_rules: list[tuple[int, int]] = []
-    orders: list[list[int]] = []
-
-    is_reading_rules = True
-    for line in lines:
-        if is_reading_rules:
-            if not line:
-                is_reading_rules = False
-                continue
-
-            rule_left, rule_right = line.split("|")
-            ordering_rules.append((int(rule_left), int(rule_right)))
-        else:
-            order = [int(x) for x in line.split(",")]
-            orders.append(order)
+    empty_line = lines.index("")
+    ordering_rules = [
+        (int(left), int(right))
+        for line in lines[:empty_line]
+        for left, right in [line.split("|")]
+    ]
+    orders = [[int(x) for x in line.split(",")] for line in lines[empty_line + 1 :]]
 
     return ordering_rules, orders
+
+def is_order_correct(order: list[int], rules: list[tuple[int, int]]) -> bool:
+    for left, right in rules:
+        if left not in order or right not in order:
+            continue
+
+        if not order.index(left) < order.index(right):
+            return False
+
+    return True
 
 def silver_solution(lines: list[str]) -> int:
     ordering_rules, orders = parse_input(lines)
 
-    order_sum = 0
-
-    for order in orders:
-        if is_order_correct(order, ordering_rules):
-            order_sum += order[len(order) // 2]
-
-    return order_sum
+    return sum(
+        order[len(order) // 2]
+        for order in orders
+        if is_order_correct(order, ordering_rules)
+    )
 
 def gold_solution(lines: list[str]) -> int:
     ordering_rules, orders = parse_input(lines)
@@ -48,15 +38,15 @@ def gold_solution(lines: list[str]) -> int:
             continue
 
         while not is_order_correct(order, ordering_rules):
-            for rule in ordering_rules:
-                if rule[0] not in order or rule[1] not in order:
+            for left, right in ordering_rules:
+                if left not in order or right not in order:
                     continue
 
-                index1 = order.index(rule[0])
-                index2 = order.index(rule[1])
+                left_index = order.index(left)
+                right_index = order.index(right)
 
-                if not index1 < index2:
-                     order[index1], order[index2] = order[index2], order[index1]
+                if not left_index < right_index:
+                    order[left_index], order[right_index] = order[right_index], order[left_index]
 
         order_sum += order[len(order) // 2]
 
