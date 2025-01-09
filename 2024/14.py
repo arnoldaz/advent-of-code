93d248c9d831d3af55efb26a8cfe809f33c10436
ArnoldaZ <c.arnas123@gmail.com>
2025-01-10 00:44:45 +0200

    refactor and implement autimatic gold solution for 2024 day 14

--- 2024/14.py
+++ 2024/14.py
@@ -1,68 +1,50 @@
-import re
-from typing import NamedTuple
-from utils.point import Point
+from dataclasses import dataclass
 
-# TODO
+from utils.point2d import Direction2d, Point2d
+from utils.string import get_ints
 
-class Robot(NamedTuple):
-    position: Point
-    velocity: Point
+@dataclass
+class Robot:
+    position: Point2d
+    velocity: Point2d
 
 def parse_input(lines: list[str]) -> list[Robot]:
-    robot_regex = r"p=(\d+),(\d+) v=(-*\d+),(-*\d+)"
-    robots: list[Robot] = []
+    return [
+        Robot(Point2d(position_x, position_y), Point2d(velocity_x, velocity_y))
+        for line in lines
+        for position_x, position_y, velocity_x, velocity_y in [get_ints(line, True)]
+    ]
 
-    for line in lines:
-        robot_match = re.match(robot_regex, line)
-        if not robot_match:
-            raise ValueError("Wrong input")
+def move_robots(robots: list[Robot], width: int, height: int, time: int):
+    for _ in range(time):
+        for robot in robots:
+            new_position = robot.position + robot.velocity
+            x, y = new_position.x, new_position.y
 
-        robot = Robot(
-            Point(int(robot_match.group(1)), int(robot_match.group(2))),
-            Point(int(robot_match.group(3)), int(robot_match.group(4)))
-        )
-        robots.append(robot)
-
-    return robots
-
-def write_to_file(robots: list[Robot], width: int, height: int, second: int):
-    with open("temp/robots2.txt", "a", encoding="utf-8") as file:
-        file.write(f"============================= SECOND {second} =============================\n")
-        for y in range(height):
-            for x in range(width):
-                a = sum(1 for robot in robots if robot.position == Point(x, y))
-                if a == 0:
-                    file.write(".")
-                else:
-                    file.write(str(a))
-            file.write("\n")
-
-def silver_solution(lines: list[str]) -> int:
-    robots = parse_input(lines)
-
-    # width = 11
-    # height = 7
-    width = 101
-    height = 103
-    end_seconds = 100
-
-    for s in range(end_seconds):
-        for i, robot in enumerate(robots):
-            pos = robot.position + robot.velocity
-            x, y = pos.x, pos.y
-            if pos.x < 0:
+            if x < 0:
                 x += width
-            elif pos.x >= width:
+            elif x >= width:
                 x -= width
-            
-            if pos.y < 0:
+
+            if y < 0:
                 y += height
-            elif pos.y >= height:
+            elif y >= height:
                 y -= height
 
-            pos = Point(x, y)
-            robots[i] = Robot(pos, robot.velocity)
+            robot.position = Point2d(x, y)
 
+def find_easter_egg(robots: list[Robot]) -> bool:
+    robot_positions = set(robot.position for robot in robots)
+    for robot in robots:
+        for i in range(1, 10):
+            if (robot.position + Direction2d.RIGHT * i) not in robot_positions:
+                break
+        else:
+            return True
+
+    return False
+
+def calculate_safety_factor(robots: list[Robot], width: int, height: int) -> int:
     half_width = width // 2
     half_height = height // 2
 
@@ -71,41 +53,31 @@ def silver_solution(lines: list[str]) -> int:
     for robot in robots:
         if robot.position.x < half_width and robot.position.y < half_height:
             q1_sum += 1
-        if robot.position.x > half_width and robot.position.y < half_height:
+        elif robot.position.x > half_width and robot.position.y < half_height:
             q2_sum += 1
-        if robot.position.x < half_width and robot.position.y > half_height:
+        elif robot.position.x < half_width and robot.position.y > half_height:
             q3_sum += 1
-        if robot.position.x > half_width and robot.position.y > half_height:
+        elif robot.position.x > half_width and robot.position.y > half_height:
             q4_sum += 1
 
     return q1_sum * q2_sum * q3_sum * q4_sum
 
+def silver_solution(lines: list[str]) -> int:
+    robots = parse_input(lines)
+    width, height = 101, 103
+    time = 100
+
+    move_robots(robots, width, height, time)
+
+    return calculate_safety_factor(robots, width, height)
+
 def gold_solution(lines: list[str]) -> int:
-    # robots = parse_input(lines)
+    robots = parse_input(lines)
+    width, height = 101, 103
 
-    # width = 101
-    # height = 103
-    # end_seconds = 10000
+    for i in range(10_000):
+        move_robots(robots, width, height, 1)
+        if find_easter_egg(robots):
+            return i + 1
 
-    # for s in range(end_seconds):
-    #     for i, robot in enumerate(robots):
-    #         pos = robot.position + robot.velocity
-    #         x, y = pos.x, pos.y
-    #         if pos.x < 0:
-    #             x += width
-    #         elif pos.x >= width:
-    #             x -= width
-            
-    #         if pos.y < 0:
-    #             y += height
-    #         elif pos.y >= height:
-    #             y -= height
-
-    #         pos = Point(x, y)
-    #         robots[i] = Robot(pos, robot.velocity)
-
-    #     write_to_file(robots, width, height, s)
-    #     print(s)
-
-    # Visually found using the text file at second 6511 (counting from 0) using the code above
-    return 6512
+    return -1
