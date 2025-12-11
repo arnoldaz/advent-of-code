b818948bba86689546a1abfa5ee3d5e92977f467
ArnoldaZ <c.arnas123@gmail.com>
2025-12-11 22:03:15 +0200

    refactor 2025 day 1

--- 2025/01.py
+++ 2025/01.py
@@ -1,53 +1,36 @@
 from utils.point2d import Direction2d
 
 def parse_input(lines: list[str]) -> list[tuple[Direction2d, int]]:
-    return [(Direction2d.from_character(line[0]), int(line[1:])) for line in lines if line]
+    return [(Direction2d.from_character(line[0]), int(line[1:])) for line in lines]
 
 def silver_solution(lines: list[str]) -> int:
     moves = parse_input(lines)
+    position = 50
+    answer = 0
 
-    current = 50
-    result = 0
+    for direction, distance in moves:
+        movement = -distance if direction == Direction2d.LEFT else distance
+        position = (position + movement) % 100
 
-    for direction, movement in moves:
-        if direction == Direction2d.LEFT:
-            current -= movement
-            while current < 0:
-                current += 100
-        elif direction == Direction2d.RIGHT:
-            current += movement
-            while current > 99:
-                current -= 100
+        if position == 0:
+            answer += 1
 
-        if current == 0:
-            result += 1
-
-    return result
+    return answer
 
 def gold_solution(lines: list[str]) -> int:
     moves = parse_input(lines)
+    position = 50
+    answer = 0
 
-    current = 50
-    result = 0
+    for direction, distance in moves:
+        full_rotations, remainder = divmod(distance, 100)
+        answer += full_rotations
+        movement = -remainder if direction == Direction2d.LEFT else remainder
+        next_position = position + movement
 
-    for direction, movement in moves:       
-        if direction == Direction2d.LEFT:
-            start = current
-            moved = False
-            current -= movement
-            while current < 0:
-                current += 100
-                result += 1
-                moved = True
-            if current == 0:
-                result += 1
-            if moved and start == 0:
-                result -= 1
+        if position != 0 and (next_position <= 0 or next_position >= 100):
+            answer += 1
 
-        elif direction == Direction2d.RIGHT:
-            current += movement
-            while current > 99:
-                current -= 100
-                result += 1
+        position = next_position % 100
 
-    return result
+    return answer
