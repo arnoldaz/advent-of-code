db4f58aa65e39c19ad840458eb00d7dfeab8c9cc
ArnoldaZ <c.arnas123@gmail.com>
2025-12-12 01:15:29 +0200

    refactor and optimize 2025 day 2

--- 2025/02.py
+++ 2025/02.py
@@ -1,31 +1,52 @@
-import re
 
 def parse_input(lines: list[str]) -> list[tuple[int, int]]:
-    line = "".join(lines)
-    ranges: list[tuple[int, int]] = []
-
-    for number_range in line.split(","):
-        start, end = number_range.split("-")
-        ranges.append((int(start), int(end)))
-
-    return ranges
-
-def find_invalid_sum(ranges: list[tuple[int, int]], allow_multiple_repeats: bool) -> int:
-    invalid_number_regex = r"^([0-9]+)\1+$" if allow_multiple_repeats else r"^([0-9]+)\1$"
-    answer = 0
-
-    for start, end in ranges:
-        for number in range(start, end + 1):
-            matches = re.match(invalid_number_regex, str(number))
-            if matches is not None:
-                answer += number
-
-    return answer
+    return [
+        (int(start), int(end))
+        for start, end
+        in (number_range.split("-") for number_range in "".join(lines).split(","))
+    ]
 
 def silver_solution(lines: list[str]) -> int:
     ranges = parse_input(lines)
-    return find_invalid_sum(ranges, False)
+
+    min_digits = len(str(min(start for start, _ in ranges)))
+    max_digits = len(str(max(end for _, end in ranges)))
+
+    values = set[int]()
+    for digits_count in range(min_digits, max_digits + 1):
+        block_length, remainder = divmod(digits_count, 2)
+        if remainder != 0:
+            continue
+
+        block_start = 10 ** (block_length - 1)
+        block_end = (10 ** block_length) - 1
+
+        for repeatable_number in range(block_start, block_end + 1):
+            possible_value = int(str(repeatable_number) * 2)
+            if any(s <= possible_value <= e for s, e in ranges):
+                values.add(possible_value)
+
+    return sum(values)
 
 def gold_solution(lines: list[str]) -> int:
     ranges = parse_input(lines)
-    return find_invalid_sum(ranges, True)
+
+    min_digits = len(str(min(start for start, _ in ranges)))
+    max_digits = len(str(max(end for _, end in ranges)))
+
+    values = set[int]()
+    for digits_count in range(min_digits, max_digits + 1):
+        for block_length in range(1, digits_count // 2 + 1):
+            repeat_amount, remainder = divmod(digits_count, block_length)
+            if remainder != 0:
+                continue
+
+            block_start = 10 ** (block_length - 1)
+            block_end = (10 ** block_length) - 1
+
+            for repeatable_number in range(block_start, block_end + 1):
+                possible_value = int(str(repeatable_number) * repeat_amount)
+                if any(s <= possible_value <= e for s, e in ranges):
+                    values.add(possible_value)
+
+    return sum(values)
