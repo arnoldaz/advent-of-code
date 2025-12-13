2b6dcb88bc0002a7b0aeb996747fa0c0d4f5501d
ArnoldaZ <c.arnas123@gmail.com>
2025-12-13 17:16:55 +0200

    refactor 2025 day 3

--- 2025/03.py
+++ 2025/03.py
@@ -1,24 +1,15 @@
 def parse_input(lines: list[str]) -> list[list[int]]:
-    number_lists: list[list[int]] = []
-    for line in lines:
-        numbers = [int(x) for x in line]
-        number_lists.append(numbers)
-
-    return number_lists
+    return [[int(symbol) for symbol in line] for line in lines]
 
 def get_total_joltage(number_lists: list[list[int]], total_digits: int) -> int:
     answer = 0
     for number_list in number_lists:
-        global_indices: list[int] = []
-        previous_index = -1
+        current_index = -1
         for i in range(total_digits - 1, -1, -1):
-            search_part = number_list[previous_index+1:len(number_list)-i]
-            digit = max(search_part)
-            global_found_indices = [index for index, value in enumerate(number_list) if value == digit]
-            corrent_index = next(index for index in global_found_indices if index not in global_indices and index > max(global_indices)) if len(global_indices) > 0 else global_found_indices[0]
-            global_indices.append(corrent_index)
+            start, end = current_index + 1, len(number_list) - i
+            digit = max(number_list[start:end])
             answer += digit * (10 ** i)
-            previous_index = corrent_index
+            current_index = number_list.index(digit, start, end)
 
     return answer
 
